b45aeed195f6689dadda8bc3a689cbff0ff15631
M1kke95 <m1kke95@hotmail.com>
2024-04-08 11:52:27 +0200

    la til harbor nuget

--- HarbNet_GuiEdition/HarbNet_GuiEdition/MauiProgram.cs
+++ HarbNet_GuiEdition/HarbNet_GuiEdition/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Gruppe8.HarbNet;
+using Microsoft.Extensions.Logging;
 
 namespace HarbNet_GuiEdition
 {
@@ -20,6 +21,7 @@ namespace HarbNet_GuiEdition
 #endif
 
             return builder.Build();
+            
         }
     }
 }
