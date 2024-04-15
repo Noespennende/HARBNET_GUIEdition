ab7e8dd9442e2c83e145ae0ba30b27c24b59a72e
Andreasberg89 <andber9@gmail.com>
2024-04-15 11:02:56 +0200

    Fjernet default maui kode og lagt til eget harbnet bilde

--- Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
+++ Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
@@ -12,22 +12,6 @@ namespace Gruppe8.HarbNet.GuiEdition
         
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-
-
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-
-        
-
         private void OnSimulationClicked(object sender, EventArgs e)
         {
             try
