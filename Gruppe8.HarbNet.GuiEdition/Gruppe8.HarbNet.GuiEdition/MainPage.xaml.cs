af5c3b1d8191900f57588e726dc3ccbdc5bed78a
M1kke95 <m1kke95@hotmail.com>
2024-04-17 21:43:30 +0200

    Flyttet create harbor til mainpage
    flyttet alt til mainpage, har ikke slettet det som var gjort tidligere i tilfellet vi støter på problemer, så det som orginalt var gjort ligger der enda

--- Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
+++ Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
@@ -1,15 +1,95 @@
 ﻿
+using Gruppe8.HarbNet.GuiEdition.ViewModel;
+
 namespace Gruppe8.HarbNet.GuiEdition
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
         Harbor harbor;
+        HarborCreateView viewModel;
+        
 
         public MainPage()
         {
-            InitializeComponent();  
-        
+            InitializeComponent();
+            viewModel = new HarborCreateView();
+            BindingContext = viewModel;
+        }
+
+        private async void OnCreateHarborClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("du klikket meg");
+                Harbor createdHarbor = CreateHarborViewModel();
+
+                if (createdHarbor != null)
+                {
+
+                    harborClick.Text = $"Du har laget en havn med ID: {harbor.ID}";
+                    
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+
+        }
+
+        private Harbor CreateHarborViewModel()
+        {
+            try
+            {
+                int numberOfSmallLoadingDocks = int.Parse(viewModel.NumberOfSmallLoadingDocks);
+                int numberOfMediumLoadingDocks = int.Parse(viewModel.NumberOfMediumLoadingDocks);
+                int numberOfLargeLoadingDocks = int.Parse(viewModel.NumberOfLargeLoadingDocks);
+                int numberOfCranesNextToLoadingDocks = int.Parse(viewModel.NumberOfCranesNextToLoadingDocks);
+                int numberOfLoadsPerCranePerHour = int.Parse(viewModel.NumberOfLoadsPerCranePerHour);
+                int numberOfCranesOnHarborStorageArea = int.Parse(viewModel.NumberOfCranesOnHarborStorageArea);
+                int numberOfSmallShipDocks = int.Parse(viewModel.NumberOfSmallShipDocks);
+                int numberOfMediumShipDocks = int.Parse(viewModel.NumberOfMediumShipDocks);
+                int numberOfLargeShipDocks = int.Parse(viewModel.NumberOfLargeShipDocks);
+                int numberOfTrucksArriveAtHarborPerHour = int.Parse(viewModel.NumberOfTrucksArriveToHarborPerHour);
+                int percentageOfContainersDirectlyLoadedFromShipToTrucks = int.Parse(viewModel.PercentageOfContainersDirectlyLoadedFromShipToTrucks);
+                int percentageOfContainersDirectlyLoadedFromHarborStorageToTrucks = int.Parse(viewModel.PercentageOfContainersDirectlyLoadedFromHarborStorageToTrucks);
+                int numberOfAdv = int.Parse(viewModel.NumberOfAdv);
+                int loadsPerAdvPerHour = int.Parse(viewModel.LoadsPerAdvPerHour);
+
+                List<Ship> listOfShips = new List<Ship>(5);
+                List<ContainerStorageRow> listOfContainerStorageRows = new List<ContainerStorageRow>(5);
+                harbor = new Harbor(listOfShips, listOfContainerStorageRows, numberOfSmallLoadingDocks, numberOfMediumLoadingDocks, numberOfLargeLoadingDocks,
+                    numberOfCranesNextToLoadingDocks, numberOfLoadsPerCranePerHour, numberOfCranesOnHarborStorageArea, numberOfSmallShipDocks,
+                    numberOfMediumShipDocks, numberOfLargeShipDocks, numberOfTrucksArriveAtHarborPerHour, percentageOfContainersDirectlyLoadedFromShipToTrucks,
+                    percentageOfContainersDirectlyLoadedFromHarborStorageToTrucks, numberOfAdv, loadsPerAdvPerHour);
+
+                return harbor;
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+            return null;
+        }
+
+        private void TextChanged(object sender, FocusEventArgs e)
+        {
+            if (sender is Entry entry)
+            {
+                if (!string.IsNullOrEmpty(entry.Text))
+                {
+                    if (!int.TryParse(entry.Text, out _))
+                    {
+
+                        DisplayAlert("Feil type", "Gi en gyldig heltallsverdi.", "OK");
+                    }
+                }
+            }
         }
 
         private void OnSimulationClicked(object sender, EventArgs e)
@@ -35,5 +115,18 @@ namespace Gruppe8.HarbNet.GuiEdition
             await Navigation.PushAsync(new HarborCreatePage());
         }
 
+
+
+  
+
+        private void NumberOfSmallLoadingDocks_Unfocused(object sender, FocusEventArgs e)
+        {
+
+        }
+
+        private void NumberOfMediumLoadingDocks_Unfocused(object sender, FocusEventArgs e)
+        {
+
+        }
     }
 }
