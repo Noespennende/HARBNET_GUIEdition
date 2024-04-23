048f95be8c7d3663ac8c04de9fc68c0134959d95
M1kke95 <m1kke95@hotmail.com>
2024-04-23 22:50:44 +0200

    Bugfix i mainpage
    Hadde glemt å legge inn storagearea. Det gjorde at skip ikke kunne hente kontainere etc.

--- Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
+++ Gruppe8.HarbNet.GuiEdition/Gruppe8.HarbNet.GuiEdition/MainPage.xaml.cs
@@ -139,22 +139,16 @@ namespace Gruppe8.HarbNet.GuiEdition
                 int percentageOfContainersDirectlyLoadedFromHarborStorageToTrucks = int.Parse(viewModel.PercentageOfContainersDirectlyLoadedFromHarborStorageToTrucks);
                 int numberOfAdv = int.Parse(viewModel.NumberOfAdv);
                 int loadsPerAdvPerHour = int.Parse(viewModel.LoadsPerAdvPerHour);
-                
-                Ship ship = new Ship("Ape", ShipSize.Large, currenttime.AddDays(1), false, 5, 5, 5);
 
-                List<Ship> shiplist = new List<Ship>(5)
-                {
-                    ship,
-                    new Ship("test1", ShipSize.Large, currenttime.AddDays(1), false, 5,5,5)
-                };
-                
-                List<ContainerStorageRow> listOfContainerStorageRows = new List<ContainerStorageRow>(5);
+                List<ContainerStorageRow> listOfContainerStorageRows = new List<ContainerStorageRow>(100);
+                ContainerStorageRow storage = new ContainerStorageRow(100);
+                listOfContainerStorageRows.Add(storage);
 
                 harbor = new Harbor(ships, listOfContainerStorageRows, numberOfSmallLoadingDocks, numberOfMediumLoadingDocks, numberOfLargeLoadingDocks,
                     numberOfCranesNextToLoadingDocks, numberOfLoadsPerCranePerHour, numberOfCranesOnHarborStorageArea, numberOfSmallShipDocks,
                     numberOfMediumShipDocks, numberOfLargeShipDocks, numberOfTrucksArriveAtHarborPerHour, percentageOfContainersDirectlyLoadedFromShipToTrucks,
                     percentageOfContainersDirectlyLoadedFromHarborStorageToTrucks, numberOfAdv, loadsPerAdvPerHour);
-                DisplayAlert("Oi", $"{shiplist.Count}", "ok");
+                
                 return harbor;
                
             }
